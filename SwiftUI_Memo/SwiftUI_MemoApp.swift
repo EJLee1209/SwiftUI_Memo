884986db25a8447d52e1a00382d41fad85c74f96
EJLee1209 <dldmswo1209@gmail.com>
2023-04-09 21:48:35 +0900

    Initial Commit

---
+++ SwiftUI_Memo/SwiftUI_MemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_MemoApp.swift
+//  SwiftUI_Memo
+//
+//  Created by 이은재 on 2023/04/09.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_MemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
